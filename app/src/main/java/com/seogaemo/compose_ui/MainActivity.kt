a507eb42b8e2a5a7380a0e97de898b2c77139d4a
JangWoojun <woojun0107@naver.com>
2024-09-10 18:36:18 +0900

    2. Button 실습

--- app/src/main/java/com/seogaemo/compose_ui/MainActivity.kt
+++ app/src/main/java/com/seogaemo/compose_ui/MainActivity.kt
@@ -1,16 +1,31 @@
 package com.seogaemo.compose_ui
 
+import android.content.Context
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Send
+import androidx.compose.material.icons.outlined.Send
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.seogaemo.compose_ui.ui.theme.Compose_UITheme
 
 class MainActivity : ComponentActivity() {
@@ -21,6 +36,7 @@ class MainActivity : ComponentActivity() {
             Compose_UITheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
+                        context = this,
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
@@ -31,17 +47,80 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting(modifier: Modifier = Modifier, context: Context? = null, name: String,) {
+//    Button(onClick = {
+//        Toast.makeText(context, "버튼 클릭", Toast.LENGTH_SHORT).show()
+//    }, modifier = modifier) {
+//        Text(text = "Hello $name!")
+//    }
+
+//    Button(onClick = {
+//        Toast.makeText(context, "버튼 클릭", Toast.LENGTH_SHORT).show()
+//    }, modifier = modifier) {
+//        Icon(imageVector = Icons.Filled.Send, contentDescription = null)
+//        Text(text = "Hello $name!")
+//    }
+
+//    Button(onClick = {
+//        Toast.makeText(context, "버튼 클릭", Toast.LENGTH_SHORT).show()
+//    }, modifier = modifier) {
+//        Icon(imageVector = Icons.Filled.Send, contentDescription = null)
+//        Spacer(modifier = Modifier.size(ButtonDefaults.IconSpacing))
+//        Text(text = "Hello $name!")
+//    }
+
+//    Button(onClick = {
+//        Toast.makeText(context, "버튼 클릭", Toast.LENGTH_SHORT).show()
+//    }, modifier = modifier, enabled = false) {
+//        Icon(imageVector = Icons.Filled.Send, contentDescription = null)
+//        Spacer(modifier = Modifier.size(ButtonDefaults.IconSpacing))
+//        Text(text = "Hello $name!")
+//    }
+
+//    Button(onClick = {
+//        Toast.makeText(context, "버튼 클릭", Toast.LENGTH_SHORT).show()
+//    }, modifier = modifier, enabled = true, border = BorderStroke(10.dp, Color.Magenta)) {
+//        Icon(imageVector = Icons.Filled.Send, contentDescription = null)
+//        Spacer(modifier = Modifier.size(ButtonDefaults.IconSpacing))
+//        Text(text = "Hello $name!")
+//    }
+
+//    Button(onClick = {
+//        Toast.makeText(context, "버튼 클릭", Toast.LENGTH_SHORT).show()
+//    }, modifier = modifier,
+//        enabled = true,
+//        border = BorderStroke(10.dp, Color.Magenta),
+//        shape = RoundedCornerShape(10.dp)
+//    ) {
+//        Icon(imageVector = Icons.Filled.Send, contentDescription = null)
+//        Spacer(modifier = Modifier.size(ButtonDefaults.IconSpacing))
+//        Text(text = "Hello $name!")
+//    }
+
+    Button(onClick = {
+        Toast.makeText(context, "버튼 클릭", Toast.LENGTH_SHORT).show()
+    }, modifier = modifier,
+        enabled = true,
+        border = BorderStroke(10.dp, Color.Magenta),
+        shape = RoundedCornerShape(10.dp),
+        contentPadding = PaddingValues(50.dp)
+    ) {
+        Icon(imageVector = Icons.Filled.Send, contentDescription = null)
+        Spacer(modifier = Modifier.size(ButtonDefaults.IconSpacing))
+        Text(text = "Hello $name!")
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Compose_UITheme {
-        Greeting("Android")
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            Greeting(
+                name = "Android",
+                modifier = Modifier.padding(innerPadding)
+            )
+        }
     }
 }

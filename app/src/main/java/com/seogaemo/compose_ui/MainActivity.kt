452c4aa85facca4621ea60b6b1e3ad733a49e4ff
JangWoojun <woojun0107@naver.com>
2024-09-11 13:57:47 +0900

    4. Surface 실습

--- app/src/main/java/com/seogaemo/compose_ui/MainActivity.kt
+++ app/src/main/java/com/seogaemo/compose_ui/MainActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.seogaemo.compose_ui.ui.theme.Compose_UITheme
 
 class MainActivity : ComponentActivity() {
@@ -32,16 +38,65 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+//    Surface {
+//        Text(
+//            text = "Hello $name!",
+//            modifier = modifier
+//        )
+//    }
+
+//    Surface(modifier = Modifier.padding(0.dp, 100.dp, 0.dp, 0.dp)) {
+//        Text(
+//            text = "Hello $name!",
+//            modifier = modifier
+//        )
+//    }
+
+//    Surface(
+//        modifier = Modifier.padding(0.dp, 100.dp, 0.dp, 0.dp),
+//        shadowElevation = 10.dp
+//    ) {
+//        Text(
+//            text = "Hello $name!",
+//            modifier = modifier
+//        )
+//    }
+
+//    Surface(
+//        modifier = Modifier.padding(0.dp, 100.dp, 0.dp, 0.dp),
+//        shadowElevation = 10.dp,
+//        border = BorderStroke(2.dp, Color.Magenta)
+//    ) {
+//        Text(
+//            text = "Hello $name!",
+//            modifier = modifier
+//        )
+//    }
+
+    Surface(
+        modifier = Modifier.padding(0.dp, 100.dp, 0.dp, 0.dp),
+        shadowElevation = 10.dp,
+        border = BorderStroke(2.dp, Color.Magenta),
+        shape = CircleShape,
+        color = MaterialTheme.colorScheme.error,
+    ) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Compose_UITheme {
-        Greeting("Android")
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            Greeting(
+                name = "Android",
+                modifier = Modifier.padding(innerPadding)
+            )
+        }
     }
 }

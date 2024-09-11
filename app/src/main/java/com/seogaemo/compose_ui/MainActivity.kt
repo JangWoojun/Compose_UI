7102153a9e00042a68501c00f892b4698ff8a17a
JangWoojun <woojun0107@naver.com>
2024-09-11 14:44:36 +0900

    6. Row 실습

--- app/src/main/java/com/seogaemo/compose_ui/MainActivity.kt
+++ app/src/main/java/com/seogaemo/compose_ui/MainActivity.kt
@@ -4,13 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.seogaemo.compose_ui.ui.theme.Compose_UITheme
 
 class MainActivity : ComponentActivity() {
@@ -32,16 +41,81 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+//    Row (modifier = Modifier.height(40.dp)) {
+//        Text(
+//            text = "Hello $name!",
+//            modifier = modifier
+//        )
+//        Text(
+//            text = "Hello $name!",
+//            modifier = modifier
+//        )
+//        Text(
+//            text = "Hello $name!",
+//            modifier = modifier
+//        )
+//    }
+
+//    Row (modifier = Modifier.height(40.dp)) {
+//        Text(
+//            text = "Hello $name!",
+//            modifier = modifier.align(alignment = Alignment.Bottom)
+//        )
+//        Text(
+//            text = "Hello $name!",
+//            modifier = modifier.align(alignment = Alignment.Top)
+//        )
+//        Text(
+//            text = "Hello $name!",
+//            modifier = modifier.align(alignment = Alignment.CenterVertically)
+//        )
+//    }
+
+//    Row (
+//        modifier = Modifier.height(40.dp),
+//        verticalAlignment = Alignment.Bottom,
+//    ) {
+//        Text(
+//            text = "Hello $name!",
+//            modifier = modifier.align(alignment = Alignment.Top)
+//        )
+//        Text(
+//            text = "Hello $name!",
+//        )
+//        Text(
+//            text = "Hello $name!",
+//        )
+//    }
+
+    Row (
+        modifier = Modifier.size(200.dp, 40.dp),
+        verticalAlignment = Alignment.Bottom,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "Hello",
+            modifier = Modifier.weight(1f).background(Color.Red)
+        )
+        Text(
+            text = "Hello",
+            modifier = Modifier.weight(2f).background(Color.Blue)
+        )
+        Text(
+            text = "Hello",
+            modifier = Modifier.weight(3f).background(Color.Magenta)
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Compose_UITheme {
-        Greeting("Android")
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            Greeting(
+                name = "Android",
+                modifier = Modifier.padding(innerPadding)
+            )
+        }
     }
 }

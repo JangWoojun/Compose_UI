7c21fdf9525c4519c419dc702c79b9888bf8bd09
JangWoojun <woojun0107@naver.com>
2024-10-15 23:04:03 +0900

    14. Slider

--- app/src/main/java/com/seogaemo/compose_ui/MainActivity.kt
+++ app/src/main/java/com/seogaemo/compose_ui/MainActivity.kt
@@ -1,39 +1,20 @@
 package com.seogaemo.compose_ui
 
 import android.os.Bundle
-import android.webkit.WebView
-import android.webkit.WebViewClient
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Call
-import androidx.compose.material.icons.filled.Info
-import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.max
-import androidx.compose.ui.viewinterop.AndroidView
 import com.seogaemo.compose_ui.ui.theme.Compose_UITheme
 
 class MainActivity : ComponentActivity() {
@@ -50,17 +31,20 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting() {
+    var sliderValue by remember { mutableFloatStateOf(0f) }
 
-    AndroidView(
-        factory = { context ->
-            WebView(context).apply {
-                webViewClient = WebViewClient()
-                settings.javaScriptEnabled = true
-                loadUrl("https://www.naver.com/")
-            }
-        },
-        modifier = Modifier.fillMaxSize()
-    )
+    Column {
+        Text(text = "Value: ${sliderValue.toInt()}")
+
+        Slider(
+            value = sliderValue,
+            onValueChange = { newValue ->
+                sliderValue = newValue
+            },
+            valueRange = 0f..100f,
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
 }
 
 @Preview(showBackground = true)
